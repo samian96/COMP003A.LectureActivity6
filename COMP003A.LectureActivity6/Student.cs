be779c3a4c44f7ada9be4c0df99ff5c4141fa27c
samian <sam.iannazzo@yahoo.com>
2026-02-17 22:27:50 -0800

    added constructor

--- COMP003A.LectureActivity6/Student.cs
+++ COMP003A.LectureActivity6/Student.cs
@@ -9,5 +9,12 @@ namespace COMP003A.LectureActivity6
         string Name;
 
         int Age;
+
+        Student(string name, int age) // If I am understanding right this is the constructor we are adding
+        {
+            Name = name;
+
+            Age = age;
+        }
     }
 }
