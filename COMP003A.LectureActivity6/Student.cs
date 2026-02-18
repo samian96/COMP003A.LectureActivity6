8f0f2821b24ba810dd23c4268bee1e6f184105d7
samian <sam.iannazzo@yahoo.com>
2026-02-17 22:23:45 -0800

    created new class / added string and int inside

---
+++ COMP003A.LectureActivity6/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.LectureActivity6
+{
+    internal class Student // This is the new class for student
+    {
+        string Name;
+
+        int Age;
+    }
+}
