f911014fba201b93b76c589cffeaae9d57f26a03
samian <sam.iannazzo@yahoo.com>
2026-02-17 23:15:05 -0800

    added comments at end of main

--- COMP003A.LectureActivity6/Program.cs
+++ COMP003A.LectureActivity6/Program.cs
@@ -12,6 +12,20 @@
             student1.DisplayInfo();
             Console.WriteLine();
             student2.DisplayInfo();
+
+           /* Each student object has its own data thanks to the use of the constructor
+            * though they are coming from the same blueprint, they are both storing their own values
+            * in the string and int if I am remembering that right.
+            */
+
+            /* Main is only calling the method to display the information,
+             * this allows for a cleaner and easier code to manage, since the 
+             * student class is public the main can call back to it
+             */
+
+            /* The data and behavior are encapsulated in the student class instead 
+             * of being part of the main.
+             */
         }
     }
 }
