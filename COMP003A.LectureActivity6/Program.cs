c4ab4f31d6b99e104a08c69d62e13dfc93003809
samian <sam.iannazzo@yahoo.com>
2026-02-17 22:18:17 -0800

    Add project files.

---
+++ COMP003A.LectureActivity6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.LectureActivity6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
